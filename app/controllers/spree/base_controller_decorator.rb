d077f1f3e5ee69cca2d5450ba424a658062ad55f
Ryan Bigg <radarlistener@gmail.com>
2012-05-09 13:18:38 +1000

    Define spree_login_path, spree_logout_path, spree_signup_path inside BaseController decorator

---
+++ app/controllers/spree/base_controller_decorator.rb
@@ -0,0 +1,14 @@
+Spree::BaseController.class_eval do
+  def spree_login_path
+    spree.login_path
+  end
+
+  def spree_signup_path
+    spree.signup_path
+  end
+
+  def spree_logout_path
+    spree.destroy_user_session_path
+  end
+end
+
