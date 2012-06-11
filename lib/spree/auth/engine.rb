beff51c403e6d38915e0b7b34412946af41ec9ff
Ryan Bigg <radarlistener@gmail.com>
2012-06-11 11:55:52 +1000

    Included Spree::AuthenticationHelpers into ApplicationController in to_prepare block

--- lib/spree/auth/engine.rb
+++ lib/spree/auth/engine.rb
@@ -12,6 +12,8 @@ module Spree
         Dir.glob(File.join(File.dirname(__FILE__), "../../../app/**/*_decorator*.rb")) do |c|
           Rails.configuration.cache_classes ? require(c) : load(c)
         end
+
+        ApplicationController.send :include, Spree::AuthenticationHelpers
       end
 
       config.to_prepare &method(:activate).to_proc
