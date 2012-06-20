d5bcc005e2b4933038802c0fec7258c575472b52
Ryan Bigg <radarlistener@gmail.com>
2012-06-21 08:21:11 +1000

    Remove useless roles association from User class
    This is now added by user_class_extensions as spree_roles

--- app/models/spree/user.rb
+++ app/models/spree/user.rb
@@ -6,7 +6,6 @@ module Spree
            :rememberable, :trackable, :validatable, :encryptable, :encryptor => 'authlogic_sha512'
 
     has_many :orders
-    has_and_belongs_to_many :roles, :join_table => 'spree_roles_users'
     belongs_to :ship_address, :foreign_key => 'ship_address_id', :class_name => 'Spree::Address'
     belongs_to :bill_address, :foreign_key => 'bill_address_id', :class_name => 'Spree::Address'
 
