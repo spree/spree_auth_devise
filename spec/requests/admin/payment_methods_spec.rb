7c462e4a2f644948d53814770043166dfbb524ec
allen13 <kex.allen13@gmail.com>
2012-07-06 07:54:48 -0400

    Fixed dismiss admin banner url helper in gateway partial.
    Fixes #5

---
+++ spec/requests/admin/payment_methods_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper'
+
+describe "Payment methods" do
+  before do
+    sign_in_as!(create(:admin_user))
+
+    visit spree.admin_path
+    click_link "Configuration"
+  end
+
+  # Regression test for #5
+  it "can dismiss the banner" do
+    Spree::User.any_instance.stub(:dismissed_banner? => false)
+    Spree::PaymentMethod.stub(:production).and_return(payment_methods = [stub])
+    payment_methods.stub(:where).and_return([])
+    click_link "Payment Methods"
+    save_and_open_page
+  end
+
+
+
+
+end
