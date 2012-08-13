0011322659a0278d33fc3187ae3ce9b334034866
Ryan Bigg <radarlistener@gmail.com>
2012-08-13 11:46:06 +1000

    Fix user + role creation in users_controller_spec

--- spec/controllers/spree/admin/users_controller_spec.rb
+++ spec/controllers/spree/admin/users_controller_spec.rb
@@ -1,40 +1,41 @@
 require 'spec_helper'
+require 'bar_ability'
 
 describe Spree::Admin::UsersController do
 
   context '#authorize_admin' do
-    let(:user) { Spree::User.new }
+    let(:user) { create(:user) }
     let(:mock_user) { mock_model Spree::User }
 
     before do
       controller.stub :spree_current_user => user
       Spree::User.stub(:find).with('9').and_return(mock_user)
       Spree::User.stub(:new).and_return(mock_user)
+      user.spree_roles.clear
     end
 
     it 'should grant access to users with an admin role' do
-      #user.stub :has_role? => true
-      user.spree_roles = [Spree::Role.find_or_create_by_name('admin')]
+      user.spree_roles << Spree::Role.find_or_create_by_name('admin')
       spree_post :index
       response.should render_template :index
     end
 
     it 'should deny access to users with an bar role' do
-      user.spree_roles = [Spree::Role.find_or_create_by_name('bar')]
+      user.spree_roles << Spree::Role.find_or_create_by_name('bar')
       Spree::Ability.register_ability(BarAbility)
       spree_post :index
       response.should render_template 'spree/shared/unauthorized'
     end
 
     it 'should deny access to users with an bar role' do
-      user.spree_roles = [Spree::Role.find_or_create_by_name('bar')]
+      user.spree_roles << Spree::Role.find_or_create_by_name('bar')
       Spree::Ability.register_ability(BarAbility)
       spree_post :update, { :id => '9' }
       response.should render_template 'spree/shared/unauthorized'
     end
 
     it 'should deny access to users without an admin role' do
-      user.stub :has_role? => false
+      user.stub :has_spree_role? => false
       spree_post :index
       response.should render_template 'spree/shared/unauthorized'
     end
