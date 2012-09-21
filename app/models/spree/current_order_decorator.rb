0f105a20a97b2cb17bb68c5a465fa5584ed98fbb
Zbigniew Pieslak <zpieslak@gmail.com>
2012-09-21 15:05:18 +0200

    Fix current_order assignment to current_user - taken form spree_auth 1.1.3
    Fixes #16

--- app/models/spree/current_order_decorator.rb
+++ app/models/spree/current_order_decorator.rb
@@ -1,4 +1,9 @@
 Spree::Core::CurrentOrder.module_eval do
+  # Associate the new order with the currently authenticated user before saving
+  def before_save_new_order
+    @current_order.user ||= try_spree_current_user
+  end
+
   def after_save_new_order
     # make sure the user has permission to access the order (if they are a guest)
     return if spree_current_user
