7a88a1cdb40f69b90ba1e640c510af2abf95ff4b
Daniel Wright <daniel@godynamo.com>
2012-10-03 16:25:28 -0400

    Use has_spree_role?(:admin) instead of spree_admin?
    Fixes #23

--- spec/controllers/spree/products_controller_spec.rb
+++ spec/controllers/spree/products_controller_spec.rb
@@ -4,13 +4,13 @@ describe Spree::ProductsController do
   let!(:product) { create(:product, :available_on => 1.year.from_now) }
 
   it "allows admins to view non-active products" do
-    controller.stub :spree_current_user => stub(:spree_admin? => true, :last_incomplete_spree_order => nil)
+    controller.stub :spree_current_user => stub(:has_spree_role? => true, :last_incomplete_spree_order => nil)
     spree_get :show, :id => product.to_param
     response.status.should == 200
   end
 
   it "cannot view non-active products" do
-    controller.stub :spree_current_user => stub(:spree_admin? => false, :last_incomplete_spree_order => nil)
+    controller.stub :spree_current_user => stub(:has_spree_role? => false, :last_incomplete_spree_order => nil)
     spree_get :show, :id => product.to_param
     response.status.should == 404
   end
