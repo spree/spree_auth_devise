11cf18b22f8a61b01344cccf3bdecb193db68295
Ryan Bigg <radarlistener@gmail.com>
2012-10-17 08:56:11 +1100

    Fix references to ControllerHelpers::CurrentOrder

--- app/controllers/spree/user_sessions_controller.rb
+++ app/controllers/spree/user_sessions_controller.rb
@@ -5,8 +5,9 @@ class Spree::UserSessionsController < Devise::SessionsController
     helper 'spree/analytics'
   end
 
-  include Spree::Core::CurrentOrder
-  include Spree::Core::ControllerHelpers
+  include Spree::Core::ControllerHelpers::Auth
+  include Spree::Core::ControllerHelpers::Common
+  include Spree::Core::ControllerHelpers::CurrentOrder
 
   ssl_required :new, :create, :destroy, :update
   ssl_allowed :login_bar
