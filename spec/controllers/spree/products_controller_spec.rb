b885e1bdcdc8eb3abce04e1d52af2bd7cb14be21
Ryan Bigg <radarlistener@gmail.com>
2013-01-31 13:26:11 +1100

    stub before_save_new_order method in ProductsController spec so that it doesn't interfere with test

--- spec/controllers/spree/products_controller_spec.rb
+++ spec/controllers/spree/products_controller_spec.rb
@@ -4,12 +4,14 @@ describe Spree::ProductsController do
   let!(:product) { create(:product, :available_on => 1.year.from_now) }
 
   it "allows admins to view non-active products" do
+    controller.stub :before_save_new_order
     controller.stub :spree_current_user => stub(:has_spree_role? => true, :last_incomplete_spree_order => nil)
     spree_get :show, :id => product.to_param
     response.status.should == 200
   end
 
   it "cannot view non-active products" do
+    controller.stub :before_save_new_order
     controller.stub :spree_current_user => stub(:has_spree_role? => false, :last_incomplete_spree_order => nil)
     spree_get :show, :id => product.to_param
     response.status.should == 404
