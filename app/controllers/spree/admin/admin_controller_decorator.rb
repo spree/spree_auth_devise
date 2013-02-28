328f00dc1b79e1273063a79e5b04a3ee5fa9ebd6
Ryan Bigg <radarlistener@gmail.com>
2013-02-28 11:27:32 -0500

    Only return a value from model_class method if constant is available
    Fixes spree/spree#2628

--- app/controllers/spree/admin/admin_controller_decorator.rb
+++ app/controllers/spree/admin/admin_controller_decorator.rb
@@ -2,6 +2,10 @@ require File.expand_path('../../base_controller_decorator', __FILE__)
 Spree::Admin::BaseController.class_eval do
   protected
     def model_class
-      "Spree::#{controller_name.classify}".constantize
+      const_name = "Spree::#{controller_name.classify}"
+      if Object.const_defined?(const_name)
+        return const_name.constantize
+      end
+      nil
     end
 end
