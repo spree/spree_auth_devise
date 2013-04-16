cf7d1e64995493fe9040b8cdc680c1713ece720a
Sean O'Hara <sohara@sohara.com>
2013-04-16 10:50:09 -0400

    Update original for user admin tab and hide the tab for users without authorization

--- app/overrides/admin_tab.rb
+++ app/overrides/admin_tab.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "user_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:users, :url => spree.admin_users_path, :icon => 'icon-user') %>",
-                     :disabled => false,
-                     :original => 'e49127029c733dcaf154ad0bd59102b63c57ac0b')
+:text => "<% if can? :admin, Spree::User %><%= tab(:users, :url => spree.admin_users_path, :icon => 'icon-user') %><% end %>",
+                     :disabled => false) #,
+                     #:original => 'e49127029c733dcaf154ad0bd59102b63c57ac0b')
 
