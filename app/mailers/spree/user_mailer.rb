a52b82232a48fe297d6f00fcba15f0c7f4ab6624
Ryan Bigg <radarlistener@gmail.com>
2013-04-26 12:53:49 +1000

    Correct preference for emails sent from

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -2,7 +2,7 @@ class Spree::UserMailer < ActionMailer::Base
   def reset_password_instructions(user)
     @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => user.reset_password_token)
 
-    mail(:to => user.email, :from => Spree::Config[:mails_from],
+    mail(:to => user.email, :from => Spree::Config[:emails_sent_from],
          :subject => Spree::Config[:site_name] + ' ' + I18n.t(:password_reset_instructions))
   end
 end
