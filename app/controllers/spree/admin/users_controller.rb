c6b21bfbecf2dcecfc0176f5eaf03ca486162a71
Ryan Bigg <radarlistener@gmail.com>
2013-05-06 12:57:13 +1000

    Fix issue where deleting a user would show a 500 error in the JS console
    Fixes #74

--- app/controllers/spree/admin/users_controller.rb
+++ app/controllers/spree/admin/users_controller.rb
@@ -73,10 +73,7 @@ module Spree
 
         def collection
           return @collection if @collection.present?
-          unless request.xhr?
-            @search = Spree::User.registered.ransack(params[:q])
-            @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
-          else
+          if request.xhr? && params[:q].present?
             #disabling proper nested include here due to rails 3.1 bug
             #@collection = User.includes(:bill_address => [:state, :country], :ship_address => [:state, :country]).
             @collection = Spree::User.includes(:bill_address, :ship_address).
@@ -88,6 +85,9 @@ module Spree
               { :search => "#{params[:q].strip}%" }).
                 limit(params[:limit] || 100)
             end
+          else
+            @search = Spree::User.registered.ransack(params[:q])
+            @collection = @search.result.page(params[:page]).per(Spree::Config[:admin_products_per_page])
           end
 
       private
