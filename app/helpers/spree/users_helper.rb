5d772a0ae404477af8ed5b31b8d5d483b9e0d725
Rocco Stanzione <grasshopper@linuxkungfu.org>
2013-05-22 15:09:30 -0500

    Speeling

--- app/helpers/spree/users_helper.rb
+++ app/helpers/spree/users_helper.rb
@@ -1,13 +1,13 @@
 module Spree
   module UsersHelper
     def password_style(user)
-      ActiveSupport::Deprecation.warn '[SPREE] Password style has be depreciated due to the removal of OpenID from the Auth Gem. '
+      ActiveSupport::Deprecation.warn '[SPREE] Password style has be deprecated due to the removal of OpenID from the Auth Gem. '
         'Please install the spree_social gem to regain this functionality and more.'
       ''
     end
 
     def openid_style(user)
-      ActiveSupport::Deprecation.warn '[SPREE] Password style has be depreciated due to the removal of OpenID from the Auth Gem. '
+      ActiveSupport::Deprecation.warn '[SPREE] Password style has be deprecated due to the removal of OpenID from the Auth Gem. '
         'Please install the spree_social gem to regain this functionality and more.'
       'display:none'
     end
