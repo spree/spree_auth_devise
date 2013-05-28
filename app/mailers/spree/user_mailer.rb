64fb2f793fb48c42ad00aa3b831e0ead88f230ca
Washington Luiz <huoxito@gmail.com>
2013-05-28 09:42:36 -0300

    UserMailer inherit from Spree::BaseMailer
    No need for check if the constant is defined since at this point users
    tracking spree_auth_devise 1-3-stable must also be tracking spree
    1-3-stable
    
    Fixes #97

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -1,8 +1,10 @@
-class Spree::UserMailer < Spree::BaseMailer
-  def reset_password_instructions(user)
-    @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => user.reset_password_token)
+module Spree
+  class UserMailer < BaseMailer
+    def reset_password_instructions(user)
+      @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => user.reset_password_token)
 
-    mail(:to => user.email, :from => from_address,
-         :subject => Spree::Config[:site_name] + ' ' + Spree.t(:password_reset_instructions))
+      mail(:to => user.email, :from => from_address,
+           :subject => Spree::Config[:site_name] + ' ' + I18n.t(:password_reset_instructions))
+    end
   end
 end
