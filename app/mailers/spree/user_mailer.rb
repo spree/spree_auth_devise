b0975e89fe6485626d57ba0aea0d3db29214d6bd
Weston Platter <westonplatter@gmail.com>
2013-06-13 23:38:56 -0600

    correctly scoped translation key

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -4,7 +4,9 @@ module Spree
       @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => user.reset_password_token)
 
       mail(:to => user.email, :from => from_address,
-           :subject => Spree::Config[:site_name] + ' ' + I18n.t(:password_reset_instructions))
+        :subject => Spree::Config[:site_name] + ' ' + 
+        I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
+  
     end
   end
 end
