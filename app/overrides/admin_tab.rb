8c884e1d9c211541768443e0b3ff31a600b5e81e
Ryan Bigg <radarlistener@gmail.com>
2013-06-17 16:54:01 +1000

    Add :original option for admin_tab override
    Fixes spree/spree#3095

--- app/overrides/admin_tab.rb
+++ app/overrides/admin_tab.rb
@@ -2,5 +2,6 @@ Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "user_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :text => "<%= tab(:users, :url => spree.admin_users_path) %>",
-                     :disabled => false)
+                     :disabled => false,
+                     :original => 'b14b1b16ac8937f99956ffef65de6f9c579ed1aa')
 
