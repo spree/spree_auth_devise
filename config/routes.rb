aaa54f083f71d163f4ce02dfb4f2682e11551995
Surya Tripathi <raj.surya19@gmail.com>
2013-07-05 16:46:32 +0530

    added named routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,8 +14,14 @@ Spree::Core::Engine.routes.prepend do
 
   devise_scope :spree_user do
     get '/login' => 'user_sessions#new', :as => :login
+    post '/login' => 'user_sessions#create', :as => :create_new_session
     get '/logout' => 'user_sessions#destroy', :as => :logout
     get '/signup' => 'user_registrations#new', :as => :signup
+    post '/signup' => 'user_registrations#create', :as => :registration
+    get '/password/recover' => 'user_passwords#new', :as => :recover_password
+    post '/password/recover' => 'user_passwords#create', :as => :reset_password
+    get '/password/change' => 'user_passwords#edit', :as => :edit_password
+    put '/password/change' => 'user_passwords#update', :as => :update_password
   end
 
   match '/checkout/registration' => 'checkout#registration', :via => :get, :as => :checkout_registration
