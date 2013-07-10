f6edfa126ced3253bdb55c0f582541dba0ee432d
Ryan Bigg <radarlistener@gmail.com>
2013-07-11 09:45:51 +1000

    Add missing end in UserMailer

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -3,8 +3,9 @@ module Spree
     def reset_password_instructions(user)
       @edit_password_reset_url = spree.edit_spree_user_password_url(:reset_password_token => user.reset_password_token)
 
-    mail(:to => user.email, :from => from_address,
-        :subject => Spree::Config[:site_name] + ' ' + 
-          I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
+      mail(:to => user.email, :from => from_address,
+          :subject => Spree::Config[:site_name] + ' ' + 
+            I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions]))
+    end
   end
 end
