2421a2d72f6be556a30a2797eed480de93840d60
Washington Luiz <huoxito@gmail.com>
2013-07-23 01:24:52 -0300

    Update deface to display login nav bar on backend
    admin layout in spree/spree was broke into partials in
    https://github.com/spree/spree/commit/032fea840560d67838ad7f8c300be44994027893
    
    Fixes #112

--- app/overrides/admin_tab.rb
+++ app/overrides/admin_tab.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/layouts/admin",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_menu",
                      :name => "user_admin_tabs",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
                      :partial => "spree/admin/users_tab",
