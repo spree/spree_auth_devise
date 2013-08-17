44ff29829877c42a370aa7e48492b5b7494e1fa3
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2013-08-17 02:22:39 -0700

    Address deprecation warnings.  Upgrade selenium webdriver version.

--- spec/features/order_spec.rb
+++ spec/features/order_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe "Orders" do
+describe "Orders", :js => true do
   it "should allow a user to view their cart at any time" do
     visit spree.cart_path
     page.should have_content("Your cart is empty")
