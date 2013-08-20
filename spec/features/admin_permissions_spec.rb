087e14305547fe20eef9688a4c800c08674d513f
Peter M. Goldstein <peter.m.goldstein@gmail.com>
2013-08-19 21:54:31 -0700

    Add explicit authorization logic for Admin::Orders::CustomerDetailsController as this is not an Admin::ResourceController
    Fixes #125

--- spec/features/admin_permissions_spec.rb
+++ spec/features/admin_permissions_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe "Admin Permissions" do
-  context "admin is restricted from accessing orders" do
-    before(:each) do
+  context 'orders' do
+    before do
       user = create(:admin_user, :email => "admin@person.com", :password => "password", :password_confirmation => "password")
       Spree::Ability.register_ability(AbilityDecorator)
       visit spree.login_path
@@ -11,15 +11,31 @@ describe "Admin Permissions" do
       click_button "Login"
     end
 
-    it "should not be able to list orders" do
-      visit spree.admin_orders_path
-      page.should have_content("Authorization Failure")
+    context "admin is restricted from accessing orders" do
+      it "should not be able to list orders" do
+        visit spree.admin_orders_path
+        page.should have_content("Authorization Failure")
+      end
+
+      it "should not be able to edit orders" do
+        create(:order, :number => "R123")
+        visit spree.edit_admin_order_path("R123")
+        page.should have_content("Authorization Failure")
+      end
     end
 
-    it "should not be able to edit orders" do
-      create(:order, :number => "R123")
-      visit spree.edit_admin_order_path("R123")
-      page.should have_content("Authorization Failure")
+    context "admin is restricted from accessing an order's customer details" do
+      let(:order) { create(:order_with_totals)}
+
+      it "should not be able to list customer details for an order" do
+        visit spree.admin_order_customer_path(order)
+        page.should have_content("Authorization Failure")
+      end
+
+      it "should not be able to edit an order's customer details" do
+        visit spree.edit_admin_order_customer_path(order)
+        page.should have_content("Authorization Failure")
+      end
     end
   end
 end
