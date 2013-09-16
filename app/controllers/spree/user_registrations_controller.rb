cbf4173c436ffd6f5f59d7da4768f242f959c826
Ryan Bigg <radarlistener@gmail.com>
2013-09-16 13:46:11 +1000

    Ensure user data is maintained even when registration form fails
    Fixes #128

--- app/controllers/spree/user_registrations_controller.rb
+++ app/controllers/spree/user_registrations_controller.rb
@@ -18,6 +18,7 @@ class Spree::UserRegistrationsController < Devise::RegistrationsController
   # GET /resource/sign_up
   def new
     super
+    @user = resource
   end
 
   # POST /resource/sign_up
