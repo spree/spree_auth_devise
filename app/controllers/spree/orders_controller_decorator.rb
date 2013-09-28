2e462c2b5e9794e7fc959b3e17b348d0952545dd
Jan Vereecken <janvereecken@clubit.be>
2013-09-28 21:48:57 +0200

    Allow multiple orders to be accessed in succession in one session
    Fixes #132
    
    Conflicts:
    	Gemfile
    
    Conflicts:
    	spec/controllers/spree/orders_controller_spec.rb

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -4,7 +4,7 @@ Spree::OrdersController.class_eval do
 
   private
     def check_authorization
-      session[:access_token] ||= params[:token]
+      session[:access_token] = params[:token] if params[:token]
       order = Spree::Order.find_by_number(params[:id]) || current_order
 
       if order
