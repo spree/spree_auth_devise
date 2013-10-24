734b207574d510738fcdb0953ed2f8499bc76826
dougbradbury <doug@8thlight.com>
2013-10-23 22:19:55 -0500

    use Spree::PermittedAttributes to allow extension

--- app/controllers/spree/users_controller.rb
+++ app/controllers/spree/users_controller.rb
@@ -39,7 +39,7 @@ class Spree::UsersController < Spree::StoreController
 
   private
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(Spree::PermittedAttributes.user_attributes)
     end
 
     def load_object
