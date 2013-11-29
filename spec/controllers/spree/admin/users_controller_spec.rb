4675422e62aac27a1baf1341667cb5156e703e8b
Washington Luiz <huoxito@gmail.com>
2013-11-29 18:35:49 -0300

    Clear out rails dynamic method deprecation warnings

--- spec/controllers/spree/admin/users_controller_spec.rb
+++ spec/controllers/spree/admin/users_controller_spec.rb
@@ -14,31 +14,25 @@ describe Spree::Admin::UsersController do
       user.spree_roles.clear
     end
 
-    it 'should grant access to users with an admin role' do
-      user.spree_roles << Spree::Role.find_or_create_by(name: 'admin')
-      spree_post :index
-      response.should render_template :index
-    end
+    context "admin" do
+      before { user.spree_roles << Spree::Role.find_or_create_by(name: 'admin') }
 
-    it "allows admins to update a user's API key" do
-      user.spree_roles << Spree::Role.find_or_create_by_name('admin')
-      mock_user.should_receive(:generate_spree_api_key!).and_return(true)
-      spree_put :generate_api_key, :id => mock_user.id
-      response.should redirect_to(spree.edit_admin_user_path(mock_user))
-    end
+      it 'should grant access to users with an admin role' do
+        spree_post :index
+        response.should render_template :index
+      end
 
-    it "allows admins to clear a user's API key" do
-      user.spree_roles << Spree::Role.find_or_create_by_name('admin')
-      mock_user.should_receive(:clear_spree_api_key!).and_return(true)
-      spree_put :clear_api_key, :id => mock_user.id
-      response.should redirect_to(spree.edit_admin_user_path(mock_user))
-    end
+      it "allows admins to update a user's API key" do
+        mock_user.should_receive(:generate_spree_api_key!).and_return(true)
+        spree_put :generate_api_key, :id => mock_user.id
+        response.should redirect_to(spree.edit_admin_user_path(mock_user))
+      end
 
-    it 'should deny access to users with an bar role' do
-      user.spree_roles << Spree::Role.find_or_create_by(name: 'bar')
-      Spree::Ability.register_ability(BarAbility)
-      spree_post :index
-      response.should redirect_to('/unauthorized')
+      it "allows admins to clear a user's API key" do
+        mock_user.should_receive(:clear_spree_api_key!).and_return(true)
+        spree_put :clear_api_key, :id => mock_user.id
+        response.should redirect_to(spree.edit_admin_user_path(mock_user))
+      end
     end
 
     it 'should deny access to users with an bar role' do
