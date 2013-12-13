286d36ec42c500fe88a7e5aeb8c8bad26300f5f6
Ryan Bigg <radarlistener@gmail.com>
2013-12-13 16:39:04 +1100

    Add installer to setup Devise's secret_token

---
+++ lib/generators/spree/auth/install/install_generator.rb
@@ -0,0 +1,25 @@
+module Spree
+  module Auth
+    module Generators
+      class InstallGenerator < Rails::Generators::Base
+        def self.source_paths
+          paths = self.superclass.source_paths
+          paths << File.expand_path('../templates', __FILE__)
+          paths.flatten
+        end
+
+        def generate_devise_key
+          template 'config/initializers/devise.rb', 'config/initializers/devise.rb'
+        end
+
+        def add_migrations
+          run 'bundle exec rake railties:install:migrations FROM=spree_auth_devise'
+        end
+
+        def run_migrations
+          run 'bundle exec rake db:migrate'
+        end
+      end
+    end
+  end
+end
