d20a08c8a6a9d1b5b00e1960d047713b232faaa9
Jeff Dutil <jdutil@burlingtonwebapps.com>
2014-03-04 22:08:35 +0000

    Conditionally add controllers & views into engine path.

---
+++ spec/features/admin/sign_out_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+feature 'Admin - Sign Out' do
+  given!(:user) do
+   create(:user,
+          email: 'email@person.com',
+          password: 'secret',
+          password_confirmation: 'secret')
+  end
+
+  background do
+    visit spree.admin_login_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    # Regression test for #1257
+    check 'Remember me'
+    click_button 'Login'
+  end
+
+  scenario 'allow a signed in user to logout' do
+    click_link 'Logout'
+    visit spree.admin_login_path
+    expect(page).to have_text 'Login'
+    expect(page).not_to have_text 'Logout'
+  end
+end
