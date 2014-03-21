974cbfaa06e4d5a924900c78eb2f8b479549273c
Edwin Cruz <softr8@gmail.com>
2014-03-21 09:44:12 -0600

    Adding support to format js when creating a new session
    Fixes #173

--- app/controllers/spree/user_sessions_controller.rb
+++ app/controllers/spree/user_sessions_controller.rb
@@ -22,13 +22,21 @@ class Spree::UserSessionsController < Devise::SessionsController
           redirect_back_or_default(after_sign_in_path_for(spree_current_user))
         }
         format.js {
-          user = resource.record
-          render :json => {:ship_address => user.ship_address, :bill_address => user.bill_address}.to_json
+          render :json => {:user => spree_current_user,
+                           :ship_address => spree_current_user.ship_address,
+                           :bill_address => spree_current_user.bill_address}.to_json
         }
       end
     else
-      flash.now[:error] = t('devise.failure.invalid')
-      render :new
+      respond_to do |format|
+        format.html {
+          flash.now[:error] = t('devise.failure.invalid')
+          render :new
+        }
+        format.js {
+          render :json => { error: t('devise.failure.invalid') }, status: :unprocessable_entity
+        }
+      end
     end
   end
 
