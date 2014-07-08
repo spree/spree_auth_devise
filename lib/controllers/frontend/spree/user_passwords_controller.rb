305cdb49c803322422e65b7686c511a2b9f451dd
Jonas Schwertfeger <jschwertfeger@gmail.com>
2014-07-08 10:39:44 +0200

    Fix bug where @spree_user is not being set before re-displaying password edit form on blank password
    Fixes #199
    Fixes #198

--- lib/controllers/frontend/spree/user_passwords_controller.rb
+++ lib/controllers/frontend/spree/user_passwords_controller.rb
@@ -36,6 +36,8 @@ class Spree::UserPasswordsController < Devise::PasswordsController
   # Fixes spree/spree#2190.
   def update
     if params[:spree_user][:password].blank?
+      self.resource = resource_class.new
+      resource.reset_password_token = params[:spree_user][:reset_password_token]
       set_flash_message(:error, :cannot_be_blank)
       render :edit
     else
