48b50e64390b77f7c0f2e88118460fa419671a83
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-07-17 01:12:01 -0400

    Fix order user assignment from cookies.

--- spec/controllers/spree/user_sessions_controller_spec.rb
+++ spec/controllers/spree/user_sessions_controller_spec.rb
@@ -9,9 +9,20 @@ describe Spree::UserSessionsController do
   context "#create" do
 
     context "using correct login information" do
+      it 'properly assigns orders user from guest_token' do
+        order1 = create(:order, guest_token: 'ABC', user_id: nil)
+        order2 = create(:order, guest_token: 'ABC', user_id: 200)
+        request.cookie_jar.signed[:guest_token] = 'ABC'
+        spree_post :create, spree_user: { email: user.email, password: 'secret' }
+
+        expect(order1.reload.user_id).to eq user.id
+        expect(order2.reload.user_id).to eq 200
+      end
+
       context "and html format is used" do
         it "redirects to default after signing in" do
           spree_post :create, spree_user: { email: user.email, password: 'secret' }
+
           expect(response).to redirect_to spree.root_path
         end
       end
