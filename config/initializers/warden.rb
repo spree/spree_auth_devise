6ec6174e2e3db12f52168e2a3efec2bb879cd67c
Washington Luiz <huoxito@gmail.com>
2014-08-03 17:14:57 -0300

    Drop order guest token when logging out

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -4,3 +4,7 @@ Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
     Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).update_all(user_id: user.id, created_by_id: user.id)
   end
 end
+
+Warden::Manager.before_logout do |user, auth, opts|
+  auth.cookies.delete :guest_token
+end
