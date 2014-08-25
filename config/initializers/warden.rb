569f4cabf1b911d9cfdf079ad0e958486b648806
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-08-25 15:27:28 -0400

    Only execute on Spree::User. Fixes #212

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,7 +1,7 @@
 # Merges users orders to their account after sign in and sign up.
 Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   if auth.cookies.signed[:guest_token].present?
-    Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).update_all(user_id: user.id, created_by_id: user.id)
+    Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).update_all(user_id: user.id, created_by_id: user.id) if user.is_a?(Spree::User)
   end
 end
 
