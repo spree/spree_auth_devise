77e167f209d08a7da65b9e98b3cc1df269e0b084
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2014-10-15 17:38:16 -0400

    Use associate_user.

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,7 +1,11 @@
 # Merges users orders to their account after sign in and sign up.
 Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   if auth.cookies.signed[:guest_token].present?
-    Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).update_all(user_id: user.id, created_by_id: user.id) if user.is_a?(Spree::User)
+    if user.is_a?(Spree::User)
+      Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
+        order.associate_user!(user)
+      end
+    end
   end
 end
 
