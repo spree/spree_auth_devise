8b089b0069a495e7d319d8d0d741df8397079240
Jack Weeden <jack@ajack.org>
2014-10-22 12:20:35 +0100

    Specs

--- spec/features/confirmation_spec.rb
+++ spec/features/confirmation_spec.rb
@@ -1,7 +1,13 @@
 require 'spec_helper'
 
 feature 'Confirmation' do
-  before { Spree::Auth::Config[:confirmable] = true }
+  before do
+    set_confirmable_option(true)
+    Spree::UserMailer.stub(:confirmation_instructions).and_return(double(deliver: true))
+  end
+
+  after(:each) { set_confirmable_option(false) }
+
   let!(:store) { create(:store) }
 
   background do
