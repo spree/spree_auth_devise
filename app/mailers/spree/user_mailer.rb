01db5e994720014221d284c9f8eaeabd54597109
Qamran Shahzad <qamran.shahzad@arkhitech.com>
2014-10-31 10:43:19 +0500

    resolved the bug in forgot passwrord

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -1,6 +1,8 @@
 module Spree
   class UserMailer < BaseMailer
     def reset_password_instructions(user, token, *args)
+         @token = token
+      @resource = user
       @edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: Spree::Store.current.url)
       @host = Spree::Store.current.url
       mail to: user.email, from: from_address, subject: Spree::Store.current.name + ' ' + I18n.t(:subject, :scope => [:devise, :mailer, :reset_password_instructions])
