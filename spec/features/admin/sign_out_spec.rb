61ece05752d03b89d4bb798b9167169fcf3e9d75
Rein Aris <info@reinaris.nl>
2014-12-10 20:55:31 +0100

    Fix allows a signed in user to logout spec

--- spec/features/admin/sign_out_spec.rb
+++ spec/features/admin/sign_out_spec.rb
@@ -16,7 +16,7 @@ RSpec.feature 'Admin - Sign Out', type: :feature do
   scenario 'allows a signed in user to logout' do
     click_link 'Logout'
     visit spree.admin_login_path
-    expect(page).to have_text 'Login'
+    expect(page).to have_button 'Login'
     expect(page).not_to have_text 'Logout'
   end
 end
