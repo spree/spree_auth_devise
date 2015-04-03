0ebdaa4b6dbe5b9a2f16c84269e86999059c8ea4
Iqra Rastee <iqra.rastee@arkhitech.com>
2015-04-03 13:06:11 +0500

    db migration to add account type

--- db/migrate/20150402104005_add_account_type_to_spree_users.rb
+++ db/migrate/20150402104005_add_account_type_to_spree_users.rb
@@ -1,6 +1,5 @@
 class AddAccountTypeToSpreeUsers < ActiveRecord::Migration
   def change
-    remove_column Spree::User, :account_type
     add_column Spree::User, :account_type, :integer, :default => 1
   end
 end
