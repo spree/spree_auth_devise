a003a15f3f643dc8c92a597f4181940f0ab4fcbc
Ben A Morgan <ben@benmorgan.io>
2015-04-11 19:09:28 -0700

    use an if statement

--- lib/controllers/frontend/spree/users_controller.rb
+++ lib/controllers/frontend/spree/users_controller.rb
@@ -1,56 +1,54 @@
-defined?(Spree::StoreController) && class Spree::UsersController < Spree::StoreController
-  skip_before_filter :set_current_order, :only => :show
-  prepend_before_filter :load_object, :only => [:show, :edit, :update]
-  prepend_before_filter :authorize_actions, :only => :new
+if defined? Spree::StoreController
+  class Spree::UsersController < Spree::StoreController
+    skip_before_filter :set_current_order, :only => :show
+    prepend_before_filter :load_object, :only => [:show, :edit, :update]
+    prepend_before_filter :authorize_actions, :only => :new
 
-  include Spree::Core::ControllerHelpers
+    include Spree::Core::ControllerHelpers
 
-  def show
-    @orders = @user.orders.complete.order('completed_at desc')
-  end
+    def show
+      @orders = @user.orders.complete.order('completed_at desc')
+    end
 
-  def create
-    @user = Spree::User.new(user_params)
-    if @user.save
+    def create
+      @user = Spree::User.new(user_params)
+      if @user.save
+        session[:guest_token] = nil if current_order
+        redirect_back_or_default(root_url)
+      else
+        render :new
+      end
+    end
 
-      if current_order
-        session[:guest_token] = nil
+    def update
+      if @user.update_attributes(user_params)
+        if params[:user][:password].present?
+          # this logic needed b/c devise wants to log us out after password changes
+          user = Spree::User.reset_password_by_token(params[:user])
+          sign_in(@user, :event => :authentication, :bypass => !Spree::Auth::Config[:signout_after_password_change])
+        end
+        redirect_to spree.account_url, :notice => Spree.t(:account_updated)
+      else
+        render :edit
+      end
+    end
+
+    private
+      def user_params
+        params.require(:user).permit(Spree::PermittedAttributes.user_attributes)
       end
 
-      redirect_back_or_default(root_url)
-    else
-      render :new
-    end
-  end
-
-  def update
-    if @user.update_attributes(user_params)
-      if params[:user][:password].present?
-        # this logic needed b/c devise wants to log us out after password changes
-        user = Spree::User.reset_password_by_token(params[:user])
-        sign_in(@user, :event => :authentication, :bypass => !Spree::Auth::Config[:signout_after_password_change])
+      def load_object
+        @user ||= spree_current_user
+        authorize! params[:action].to_sym, @user
       end
-      redirect_to spree.account_url, :notice => Spree.t(:account_updated)
-    else
-      render :edit
-    end
+
+      def authorize_actions
+        authorize! params[:action].to_sym, Spree::User.new
+      end
+
+      def accurate_title
+        Spree.t(:my_account)
+      end
   end
-
-  private
-    def user_params
-      params.require(:user).permit(Spree::PermittedAttributes.user_attributes)
-    end
-
-    def load_object
-      @user ||= spree_current_user
-      authorize! params[:action].to_sym, @user
-    end
-
-    def authorize_actions
-      authorize! params[:action].to_sym, Spree::User.new
-    end
-
-    def accurate_title
-      Spree.t(:my_account)
-    end
 end
