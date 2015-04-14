92155f9517e54815a72c43c444b121d2ad99a29f
Iqra Rastee <iqra.rastee@arkhitech.com>
2015-04-14 13:10:17 +0500

    new user session url removed

--- lib/custom_failure_app.rb
+++ lib/custom_failure_app.rb
@@ -1,8 +1,4 @@
  class CustomFailureApp < Devise::FailureApp
-    def redirect_url
-       new_user_session_url(:subdomain => 'secure')
-    end
-
     # You need to override respond to eliminate recall
     def respond
       if http_auth?
