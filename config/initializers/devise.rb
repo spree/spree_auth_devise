444ca4f2d6be0efbd35031924a12c6521b907a3d
Iqra Rastee <iqra.rastee@arkhitech.com>
2015-04-15 16:21:27 +0500

    unconfirmed authorization reestablished

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -50,7 +50,7 @@ Devise.setup do |config|
   # without confirming the account, but blocking it after a certain period
   # (ie 2 days).
 #  config.confirm_within = 5.days
-#  config.allow_unconfirmed_access_for = 5.days
+  config.allow_unconfirmed_access_for = 5.days
 
   # ==> Configuration for :rememberable
   # The time the user will be remembered without asking for credentials again.
--- lib/custom_failure_app.rb
+++ lib/custom_failure_app.rb
@@ -5,8 +5,9 @@
         http_auth
       else
         if request.format == "application/json"
-          self.status = 422 
+          self.status = 401 
           self.content_type = "json"
+          self.headers['WWW-Authenticate']="Please Confirm Your Account To Continue"
           self.response_body = {error: I18n.t("devise.failure.inactive")}.to_json
         else
           redirect          
