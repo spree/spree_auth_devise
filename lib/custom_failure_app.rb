3d14b1cbc48c74ee56ccc8e87191e65c70f42876
Iqra Rastee <iqra.rastee@arkhitech.com>
2015-04-15 12:05:49 +0500

    testing response

--- lib/custom_failure_app.rb
+++ lib/custom_failure_app.rb
@@ -4,10 +4,10 @@
       if http_auth?
         http_auth
       else
-        if request.format == 'application/json'
-          self.status = 401 
-          self.content_type = 'json'
-          self.response_body = {error: I18n.t('devise.failure.inactive')}.to_json
+        if request.format == "application/json"
+          self.status = 422 
+          self.content_type = "json"
+          self.response_body = {error: I18n.t("devise.failure.inactive")}.to_json
         else
           redirect          
         end
