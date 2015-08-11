336b0e4f3a9c38dba36c8deef37549f48f61672f
Jeff Dutil <JDutil@BurlingtonWebApps.com>
2015-08-10 20:36:10 -0400

    Only tie orders w/users email. Fixes spree/spree#6578

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -2,7 +2,7 @@
 Warden::Manager.after_set_user except: :fetch do |user, auth, opts|
   if auth.cookies.signed[:guest_token].present?
     if user.is_a?(Spree::User)
-      Spree::Order.where(guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
+      Spree::Order.where(email: user.email, guest_token: auth.cookies.signed[:guest_token], user_id: nil).each do |order|
         order.associate_user!(user)
       end
     end
