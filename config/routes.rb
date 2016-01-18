30cd1611e0b27f0e7572ebc5b09f1d34f81f037d
Vishal Zambre <vishal.zambre@weboniselab.com>
2016-01-18 17:52:16 +0530

    draw routes conditionally as per spree_frontend and spree_backend engine.
    add missing admin routes for recover password if frontend is not available
    
    change routes path in admin views if frontend is not available
    
    corrected helper file path for admin controllers
    
    add reset password instruction for admin if frontend not available

--- config/routes.rb
+++ config/routes.rb
@@ -1,48 +1,55 @@
 Spree::Core::Engine.add_routes do
-  devise_for :spree_user,
-             :class_name => 'Spree::User',
-             :controllers => { :sessions => 'spree/user_sessions',
-                               :registrations => 'spree/user_registrations',
-                               :passwords => 'spree/user_passwords',
-                               :confirmations => 'spree/user_confirmations' },
-             :skip => [:unlocks, :omniauth_callbacks],
-             :path_names => { :sign_out => 'logout' },
-             :path_prefix => :user
-
-  resources :users, :only => [:edit, :update]
-
-  devise_scope :spree_user do
-    get '/login' => 'user_sessions#new', :as => :login
-    post '/login' => 'user_sessions#create', :as => :create_new_session
-    get '/logout' => 'user_sessions#destroy', :as => :logout
-    get '/signup' => 'user_registrations#new', :as => :signup
-    post '/signup' => 'user_registrations#create', :as => :registration
-    get '/password/recover' => 'user_passwords#new', :as => :recover_password
-    post '/password/recover' => 'user_passwords#create', :as => :reset_password
-    get '/password/change' => 'user_passwords#edit', :as => :edit_password
-    put '/password/change' => 'user_passwords#update', :as => :update_password
-    get '/confirm' => 'user_confirmations#show', :as => :confirmation if Spree::Auth::Config[:confirmable]
-  end
-
-  get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
-  put '/checkout/registration' => 'checkout#update_registration', :as => :update_checkout_registration
-
-  resource :account, :controller => 'users'
-
-  namespace :admin do
+  if Spree::Auth::Engine.frontend_available?
     devise_for :spree_user,
                :class_name => 'Spree::User',
-               :controllers => { :sessions => 'spree/admin/user_sessions',
-                                 :passwords => 'spree/admin/user_passwords' },
-               :skip => [:unlocks, :omniauth_callbacks, :registrations],
+               :controllers => { :sessions => 'spree/user_sessions',
+                                 :registrations => 'spree/user_registrations',
+                                 :passwords => 'spree/user_passwords',
+                                 :confirmations => 'spree/user_confirmations' },
+               :skip => [:unlocks, :omniauth_callbacks],
                :path_names => { :sign_out => 'logout' },
                :path_prefix => :user
+
+    resources :users, :only => [:edit, :update]
+
     devise_scope :spree_user do
-      get '/authorization_failure', :to => 'user_sessions#authorization_failure', :as => :unauthorized
       get '/login' => 'user_sessions#new', :as => :login
       post '/login' => 'user_sessions#create', :as => :create_new_session
       get '/logout' => 'user_sessions#destroy', :as => :logout
+      get '/signup' => 'user_registrations#new', :as => :signup
+      post '/signup' => 'user_registrations#create', :as => :registration
+      get '/password/recover' => 'user_passwords#new', :as => :recover_password
+      post '/password/recover' => 'user_passwords#create', :as => :reset_password
+      get '/password/change' => 'user_passwords#edit', :as => :edit_password
+      put '/password/change' => 'user_passwords#update', :as => :update_password
+      get '/confirm' => 'user_confirmations#show', :as => :confirmation if Spree::Auth::Config[:confirmable]
     end
 
+    get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
+    put '/checkout/registration' => 'checkout#update_registration', :as => :update_checkout_registration
+
+    resource :account, :controller => 'users'
+  end
+
+  if Spree::Auth::Engine.backend_available?
+    namespace :admin do
+      devise_for :spree_user,
+                 :class_name => 'Spree::User',
+                 :controllers => { :sessions => 'spree/admin/user_sessions',
+                                   :passwords => 'spree/admin/user_passwords' },
+                 :skip => [:unlocks, :omniauth_callbacks, :registrations],
+                 :path_names => { :sign_out => 'logout' },
+                 :path_prefix => :user
+      devise_scope :spree_user do
+        get '/authorization_failure', :to => 'user_sessions#authorization_failure', :as => :unauthorized
+        get '/login' => 'user_sessions#new', :as => :login
+        post '/login' => 'user_sessions#create', :as => :create_new_session
+        get '/logout' => 'user_sessions#destroy', :as => :logout
+        get '/password/recover' => 'user_passwords#new', as: :recover_password
+        post '/password/recover' => 'user_passwords#create', as: :reset_password
+        get '/password/change' => 'user_passwords#edit', as: :edit_password
+        put '/password/change' => 'user_passwords#update', as: :update_password
+      end
+    end
   end
 end
