afb88d92ee6d55dd51fcfe375bb20ded6192301f
Augusto Goldoni <augusto@jaya.tech>
2018-05-22 18:19:31 -0300

    Makes Devise validatable be configurable

--- app/models/spree/auth_configuration.rb
+++ app/models/spree/auth_configuration.rb
@@ -3,5 +3,6 @@ module Spree
     preference :registration_step, :boolean, default: true
     preference :signout_after_password_change, :boolean, default: true
     preference :confirmable, :boolean, default: false
+    preference :validatable, :boolean, default: true
   end
 end
