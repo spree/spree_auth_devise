4454212ea6293dab00ca37286c8d30b78846719d
VygovskySergey <vygovskysergey@gmail.com>
2018-07-13 11:45:24 +0200

    Fix associating order to user problem due to new token

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,8 +1,13 @@
 # Merges users orders to their account after sign in and sign up.
 Warden::Manager.after_set_user except: :fetch do |user, auth, _opts|
   guest_token = auth.cookies.signed[:guest_token]
+  token       = auth.cookies.signed[:token]
 
-  if guest_token.present? && user.is_a?(Spree::User)
+  if token.present? && user.is_a?(Spree::User)
+    Spree::Order.incomplete.where(token: token, user_id: nil).each do |order|
+      order.associate_user!(user)
+    end
+  elsif guest_token.present? && user.is_a?(Spree::User)
     Spree::Order.incomplete.where(guest_token: guest_token, user_id: nil).each do |order|
       order.associate_user!(user)
     end
