f408ced02d8576e2f0082ec233d269380cab06b8
Eric Kurzenberger <erick@mitchells.com>
2019-06-27 14:03:25 -0400

    Fixes an issue when upgrading to Spree 3.7 from an earlier version where a previous user may have the  cookie present, but the  column on  is no longer present (as it was renamed to  in 3.7)

--- config/initializers/warden.rb
+++ config/initializers/warden.rb
@@ -1,18 +1,14 @@
 # Merges users orders to their account after sign in and sign up.
 Warden::Manager.after_set_user except: :fetch do |user, auth, _opts|
-  guest_token = auth.cookies.signed[:guest_token]
-  token       = auth.cookies.signed[:token]
+  token = auth.cookies.signed[:guest_token] || auth.cookies.signed[:token]
+  token_attr = Spree::Order.has_attribute?(:token) ? :token : :guest_token
 
   if token.present? && user.is_a?(Spree::User)
-    Spree::Order.incomplete.where(token: token, user_id: nil).each do |order|
-      order.associate_user!(user)
-    end
-  elsif guest_token.present? && user.is_a?(Spree::User)
-    Spree::Order.incomplete.where(guest_token: guest_token, user_id: nil).each do |order|
+    Spree::Order.incomplete.where(token_attr => token, user_id: nil).each do |order|
       order.associate_user!(user)
     end
   end
-end
+end 
 
 Warden::Manager.before_logout do |_user, auth, _opts|
   auth.cookies.delete(:guest_token)
