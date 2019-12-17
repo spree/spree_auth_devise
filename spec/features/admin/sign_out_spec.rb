e0a8ef403b7e88eb5ccb86fefb923695e73c740a
Robert Nowakowski <aplegatt@gmail.com>
2019-12-17 16:33:53 +0100

    Fixes admin sign out specs

--- spec/features/admin/sign_out_spec.rb
+++ spec/features/admin/sign_out_spec.rb
@@ -9,11 +9,11 @@ RSpec.feature 'Admin - Sign Out', type: :feature do
     fill_in 'Password', with: 'secret'
     # Regression test for #1257
     check 'Remember me'
-    click_button 'Log in'
+    click_button 'Login'
   end
 
   scenario 'allows a signed in user to logout', js: true do
-    click_link 'Logout'
+    log_out
     visit spree.admin_login_path
     expect(page).to have_button 'Login'
     expect(page).not_to have_text 'Logout'
