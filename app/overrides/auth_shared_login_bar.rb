0c5ab929cd07aaef9682ac9bf9766d92a0f18b56
Damian Legawiec <damian@sparksolutions.co>
2020-01-12 12:17:39 +0100

    Removed deprecated override
    This was moved into Spree in 4.1

--- app/overrides/auth_shared_login_bar.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(virtual_path: "spree/shared/_nav_bar",
-                     name: "auth_shared_login_bar",
-                     insert_before: "li#search-bar",
-                     partial: "spree/shared/login_bar",
-                     disabled: false,
-                     original: 'c00d67dfc0496d21a351b48c41a728412cb5ed2c')
