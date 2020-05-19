52456181685732babbc8ee0f834802fffd74940d
Oleg Leontiev <oleg.leontiev@sparksolutions.co>
2020-05-19 13:08:22 +0200

    use error struct

--- lib/controllers/frontend/spree/api/v2/storefront/account_controller_decorator.rb
+++ lib/controllers/frontend/spree/api/v2/storefront/account_controller_decorator.rb
@@ -8,16 +8,34 @@ module Spree
           end
 
           def create
-            user = Spree.user_class.new_with_session(spree_user_params, session)
+            user = Spree.user_class.new(spree_user_params)
             if user.save
               render_serialized_payload { serialize_resource(user) }
             else
-              render json: { errors: user.errors.messages }
+              render_error_payload(structed_error(user.errors))
             end
           end
 
           def spree_user_params
             params.require(:user).permit(Spree::PermittedAttributes.user_attributes)
+          end
+
+          def structed_error(error)
+            struct_error = Struct.new(:error) do
+              def to_s
+                return error.full_messages.join(', ') if error&.respond_to?(:full_messages)
+
+                value.to_s
+              end
+
+              def to_h
+                return error.messages if error&.respond_to?(:messages)
+
+                {}
+              end
+            end
+
+            struct_error.new(error)
           end
         end
       end
