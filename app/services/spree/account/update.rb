c046eb307ce8b888ff0ed002fd59f7d738b70087
v10110 <virginia.nguyen@sparksolutions.co>
2020-05-19 18:52:49 +0200

    Storefront account update

---
+++ app/services/spree/account/update.rb
@@ -0,0 +1,17 @@
+module Spree
+  module Account
+    class Update
+      prepend Spree::ServiceModule::Base
+
+      def call(user:, user_params: nil)
+        user_params ||= {}
+
+        if user.update(user_params)
+          success(user)
+        else
+          failure(user)
+        end
+      end
+    end
+  end
+end
