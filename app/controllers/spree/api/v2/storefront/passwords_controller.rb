777b63b1201c91e07dd74bde527cb24ea0ab8444
Piotr Milewski <piotr.milewski@sparksolutions.co>
2020-05-21 09:46:31 +0200

    Reset Password API v2 endpoints (#490)
    Co-authored-by: Damian Legawiec <damian@sparksolutions.co>

---
+++ app/controllers/spree/api/v2/storefront/passwords_controller.rb
@@ -0,0 +1,33 @@
+module Spree
+  module Api
+    module V2
+      module Storefront
+        class PasswordsController < ::Devise::PasswordsController
+          def create
+            user = Spree.user_class.find_by(email: params[:user][:email])
+
+            if user&.send_reset_password_instructions
+              head :ok
+            else
+              head :not_found
+            end
+          end
+
+          def update
+            user = Spree.user_class.reset_password_by_token(
+              password: params[:user][:password],
+              password_confirmation: params[:user][:password_confirmation],
+              reset_password_token: params[:id]
+            )
+
+            if user.errors.empty?
+              head :ok
+            else
+              render json: { error: user.errors.full_messages.to_sentence }, status: :unprocessable_entity
+            end
+          end
+        end
+      end
+    end
+  end
+end
