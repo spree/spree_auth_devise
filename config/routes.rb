662e29a4da183687809a67810031a1fba4c25d5a
Vasyl Maksymov <vasyl.maksymov@terracycle.com>
2020-05-27 18:00:21 +0300

    Remove overridden route

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,6 @@ Spree::Core::Engine.add_routes do
   get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
   put '/checkout/registration' => 'checkout#update_registration', :as => :update_checkout_registration
 
-  get '/account_link' => 'store#account_link'
   resource :account, controller: 'users'
 
   namespace :admin, path: Spree.admin_path do
