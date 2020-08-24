5bb817e3a852258b7c70393c4ec788e67b9beba9
Krzysztof Kalisz <krzysztof.kalisz@sparksolutions.co>
2020-08-24 10:05:18 +0200

    SD-934 Use email template in reset password email

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -6,6 +6,7 @@ module Spree
       @locale = @current_store.has_attribute?(:default_locale) ? @current_store.default_locale : I18n.default_locale
       I18n.locale = @locale if @locale.present?
       @edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: @current_store.url)
+      @user = user
 
       mail to: user.email, from: from_address, subject: @current_store.name + ' ' + I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions])
     end
