a7d4936088d98d467696a08e7e4ac2d43afc08f9
Tegan Rauh <tegan.rauh@shiftdsm.com>
2020-09-23 09:30:05 -0500

    Single quote

--- spec/support/register_helpers.rb
+++ spec/support/register_helpers.rb
@@ -2,7 +2,7 @@ module RegisterHelpers
   def set_registerable_option(value)
     Spree::Auth::Config[:registerable] = value
     Spree.send(:remove_const, 'User')
-    load File.expand_path("../../../app/models/spree/user.rb", __FILE__)
+    load File.expand_path('../../../app/models/spree/user.rb', __FILE__)
   end
 end
 
