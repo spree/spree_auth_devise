b3bef61c8560624b4cab9e9dfc77eb70a1e67918
Robert Nowakowski <aplegatt@gmail.com>
2020-11-17 14:04:35 +0100

    [SD-1004] Changes emails urls to contain current store url

--- app/mailers/spree/user_mailer.rb
+++ app/mailers/spree/user_mailer.rb
@@ -8,7 +8,7 @@ module Spree
       @edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: @current_store.url)
       @user = user
 
-      mail to: user.email, from: from_address, subject: @current_store.name + ' ' + I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions])
+      mail to: user.email, from: from_address, subject: @current_store.name + ' ' + I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions]), store_url: @current_store.url
     end
 
     def confirmation_instructions(user, token, _opts = {})
