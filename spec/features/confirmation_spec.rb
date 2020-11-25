66250fb46e0bba58bcdc243e4b0dc6a69b2cbe18
Robert Nowakowski <aplegatt@gmail.com>
2020-11-25 13:57:26 +0100

    Adds and changes tests

--- spec/features/confirmation_spec.rb
+++ spec/features/confirmation_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.feature 'Confirmation', type: :feature, reload_user: true do
   before do
     set_confirmable_option(true)
-    Spree::UserMailer.stub(:confirmation_instructions).and_return(double(deliver: true))
+    expect(Spree::UserMailer).to receive(:confirmation_instructions).with(anything, anything, { current_store_id: Spree::Store.current.id }).and_return(double(deliver: true))
   end
 
   after(:each) { set_confirmable_option(false) }
