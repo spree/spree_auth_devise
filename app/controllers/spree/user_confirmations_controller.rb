9c84749f73db217179e0bf196a270072bf9b95ed
Damian Legawiec <damian@sparksolutions.co>
2021-01-16 15:42:20 +0100

    Pass current_store only on Spree 4.2+

--- app/controllers/spree/user_confirmations_controller.rb
+++ app/controllers/spree/user_confirmations_controller.rb
@@ -8,15 +8,17 @@ class Spree::UserConfirmationsController < Devise::ConfirmationsController
 
   before_action :set_current_order
 
-  # POST /resource/confirmation
-  def create
-    self.resource = resource_class.send_confirmation_instructions(resource_params, current_store)
-    yield resource if block_given?
+  if Spree.version.to_f >= 4.2
+    # POST /resource/confirmation
+    def create
+      self.resource = resource_class.send_confirmation_instructions(resource_params, current_store)
+      yield resource if block_given?
 
-    if successfully_sent?(resource)
-      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
-    else
-      respond_with(resource)
+      if successfully_sent?(resource)
+        respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+      else
+        respond_with(resource)
+      end
     end
   end
 
