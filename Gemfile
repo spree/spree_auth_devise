4920f2f2e16dda64caaa438624199b60c943ebbe
Agnieszka Jacek <agnieszka.jacek90@gmail.com>
2021-02-04 19:11:53 +0100

    [SD-1142] Headless mode fixes (#520)
    * Redirect to admin login page after admin logout
    
    * Redirect to admin recover password wheb clicking Forgot Password on Admin Login page
    
    * Remove pry

--- Gemfile
+++ Gemfile
@@ -3,4 +3,5 @@ source 'https://rubygems.org'
 gem 'rails-controller-testing'
 gem 'spree', github: 'spree/spree', branch: 'master'
 
+gem 'pry', '~> 0.13.1'
 gemspec
--- lib/controllers/backend/spree/admin/user_sessions_controller.rb
+++ lib/controllers/backend/spree/admin/user_sessions_controller.rb
@@ -59,4 +59,20 @@ class Spree::Admin::UserSessionsController < Devise::SessionsController
       "spree/layouts/admin"
     end
   end
+
+  def respond_to_on_destroy
+    # We actually need to hardcode this as Rails default responder doesn't
+    # support returning empty response on GET request
+    respond_to do |format|
+      format.all { head :no_content }
+      format.any(*navigational_formats) { redirect_to after_sign_out_redirect(resource_name) }
+    end
+  end
+
+  def after_sign_out_redirect(resource_or_scope)
+    scope = Devise::Mapping.find_scope!(resource_or_scope)
+    router_name = Devise.mappings[scope].router_name
+    context = router_name ? send(router_name) : self
+    context.respond_to?(:admin_login_path) ? context.admin_login_path : "/"
+  end
 end
