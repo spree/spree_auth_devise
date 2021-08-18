b096b712d30e7ba21befe35f82892775ec53fda9
Matthew Kennedy <m.kennedy@me.com>
2021-08-18 07:57:43 +0100

    Update Spree.t(:logged_in_successfully) to match spelling in main locale file. (#542)

--- lib/controllers/backend/spree/admin/user_sessions_controller.rb
+++ lib/controllers/backend/spree/admin/user_sessions_controller.rb
@@ -14,7 +14,7 @@ class Spree::Admin::UserSessionsController < Devise::SessionsController
     if spree_user_signed_in?
       respond_to do |format|
         format.html {
-          flash[:success] = Spree.t(:logged_in_succesfully)
+          flash[:success] = Spree.t(:logged_in_successfully)
           redirect_back_or_default(after_sign_in_path_for(spree_current_user))
         }
         format.js {
