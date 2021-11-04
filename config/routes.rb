6f603bb44e22e027691ce8e12b52a3ba4e78b1ee
Damian Legawiec <damian@sparksolutions.co>
2021-11-04 13:43:19 +0100

    Fixed conditional routes when Admin is available and no Frontend

--- config/routes.rb
+++ config/routes.rb
@@ -1,33 +1,31 @@
 Spree::Core::Engine.add_routes do
+  devise_for :spree_user,
+            class_name: Spree.user_class.to_s,
+            controllers: { sessions: 'spree/user_sessions',
+                              registrations: 'spree/user_registrations',
+                              passwords: 'spree/user_passwords',
+                              confirmations: 'spree/user_confirmations' },
+            skip: [:unlocks, :omniauth_callbacks],
+            path_names: { sign_out: 'logout' },
+            path_prefix: :user
+
+  devise_scope :spree_user do
+    get '/login' => 'user_sessions#new', :as => :login
+    post '/login' => 'user_sessions#create', :as => :create_new_session
+    get '/logout' => 'user_sessions#destroy', :as => :logout
+    get '/signup' => 'user_registrations#new', :as => :signup
+    post '/signup' => 'user_registrations#create', :as => :registration
+    get '/password/recover' => 'user_passwords#new', :as => :recover_password
+    post '/password/recover' => 'user_passwords#create', :as => :reset_password
+    get '/password/change' => 'user_passwords#edit', :as => :edit_password
+    put '/password/change' => 'user_passwords#update', :as => :update_password
+    get '/confirm' => 'user_confirmations#show', :as => :confirmation
+  end
+
   if Spree::Core::Engine.frontend_available?
-    devise_for :spree_user,
-              class_name: Spree.user_class.to_s,
-              controllers: { sessions: 'spree/user_sessions',
-                                registrations: 'spree/user_registrations',
-                                passwords: 'spree/user_passwords',
-                                confirmations: 'spree/user_confirmations' },
-              skip: [:unlocks, :omniauth_callbacks],
-              path_names: { sign_out: 'logout' },
-              path_prefix: :user
-
     resources :users, only: [:edit, :update]
-
-    devise_scope :spree_user do
-      get '/login' => 'user_sessions#new', :as => :login
-      post '/login' => 'user_sessions#create', :as => :create_new_session
-      get '/logout' => 'user_sessions#destroy', :as => :logout
-      get '/signup' => 'user_registrations#new', :as => :signup
-      post '/signup' => 'user_registrations#create', :as => :registration
-      get '/password/recover' => 'user_passwords#new', :as => :recover_password
-      post '/password/recover' => 'user_passwords#create', :as => :reset_password
-      get '/password/change' => 'user_passwords#edit', :as => :edit_password
-      put '/password/change' => 'user_passwords#update', :as => :update_password
-      get '/confirm' => 'user_confirmations#show', :as => :confirmation
-    end
-
     get '/checkout/registration' => 'checkout#registration', :as => :checkout_registration
     put '/checkout/registration' => 'checkout#update_registration', :as => :update_checkout_registration
-
     resource :account, controller: 'users'
   end
 
