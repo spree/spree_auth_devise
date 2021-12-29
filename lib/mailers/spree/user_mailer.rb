3cf283ede1b6d7fd423675a3d64397de6a6f208a
Damian Legawiec <damian@getvendo.com>
2021-12-29 13:25:19 +0100

    fix

--- lib/mailers/spree/user_mailer.rb
+++ lib/mailers/spree/user_mailer.rb
@@ -8,7 +8,7 @@ module Spree
       @edit_password_reset_url = spree.edit_spree_user_password_url(reset_password_token: token, host: @current_store.url)
       @user = user
 
-      mail to: user.email, from: mail_from_address, reply_to: reply_to_address,
+      mail to: user.email, from: from_address, reply_to: reply_to_address,
            subject: @current_store.name + ' ' + I18n.t(:subject, scope: [:devise, :mailer, :reset_password_instructions]), store_url: @current_store.url
     end
 
@@ -18,7 +18,7 @@ module Spree
       @confirmation_url = spree.confirmation_url(confirmation_token: token, host: @current_store.url)
       @email = user.email
 
-      mail to: user.email, from: mail_from_address, reply_to: reply_to_address,
+      mail to: user.email, from: from_address, reply_to: reply_to_address,
            subject: @current_store.name + ' ' + I18n.t(:subject, scope: [:devise, :mailer, :confirmation_instructions]), store_url: @current_store.url
     end
   end
