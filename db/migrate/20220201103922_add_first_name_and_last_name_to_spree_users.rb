9e8788207d256b6f9f43dc6cfa04cec2008e635f
Damian Legawiec <damian@sparksolutions.co>
2022-02-03 16:06:34 +0100

    Fix when columns already exist

--- db/migrate/20220201103922_add_first_name_and_last_name_to_spree_users.rb
+++ db/migrate/20220201103922_add_first_name_and_last_name_to_spree_users.rb
@@ -1,6 +1,6 @@
 class AddFirstNameAndLastNameToSpreeUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :spree_users, :first_name, :string
-    add_column :spree_users, :last_name, :string
+    add_column :spree_users, :first_name, :string unless column_exists?(:spree_users, :first_name)
+    add_column :spree_users, :last_name, :string unless column_exists?(:spree_users, :last_name)
   end
 end
